e1ef6e807f6d15242fa4f4fca827698d131d26da
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-26 08:59:19 -0600

    Initial commit

--- src/Ejercicio1CantidadPalabrasFrase.java
+++ src/Ejercicio1CantidadPalabrasFrase.java
@@ -1,10 +1,3 @@
-public class Main {
-    public static void main(String[] args) {
+public class Ejercicio1CantidadPalabrasFrase {
 
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-    }
 }
