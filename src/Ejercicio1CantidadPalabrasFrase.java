0e1ef441cb834546308d268f6c5f4bdbf1c1930f
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-28 23:04:32 -0600

    Gabriel Ernesto Obando García - Ejercicio 1 finalizado

--- src/Ejercicio1CantidadPalabrasFrase.java
+++ src/Ejercicio1CantidadPalabrasFrase.java
@@ -1,3 +1,16 @@
-public class Ejercicio1CantidadPalabrasFrase {
+import java.util.Scanner;
 
+public class Ejercicio1CantidadPalabrasFrase {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        String frase;
+
+        System.out.println("Ingrese una frase: ");
+        frase = teclado.nextLine();
+
+        String[] palabras = frase.split(" ");
+
+        System.out.println("La frase contiene " + palabras.length + " palabras");
+    }
 }
