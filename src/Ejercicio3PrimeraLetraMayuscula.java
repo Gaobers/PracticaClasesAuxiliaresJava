1e2c908d5b7ddad52d4ad7df8a676cfd3d4d2c0d
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-28 23:17:56 -0600

    Gabriel Ernesto Obando García - Ejercicio 3 finalizado

--- src/Ejercicio3PrimeraLetraMayuscula.java
+++ src/Ejercicio3PrimeraLetraMayuscula.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Ejercicio3PrimeraLetraMayuscula {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        String frase;
+        System.out.println("Ingresa una frase: ");
+        frase = teclado.nextLine();
+
+        String[] palabras = frase.split(" ");
+        String resultado = "";
+
+        for (int i = 0 ; i < palabras.length; i++){
+            resultado += palabras[i].substring(0, 1).toUpperCase() + palabras[i].substring(1) + " ";
+        }
+
+        System.out.println("Resultado: " + resultado);
+    }
 }
